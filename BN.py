d5a6fdba603d672781dc20ea699b1a2ef5dd6115
PepelNaTishke <chpunkpas09@gmail.com>
2026-03-24 23:59:18 +0300

    Update BN.py

--- BN.py
+++ BN.py
@@ -1,65 +1,83 @@
 from math import gcd
 
-def lcm(a, b):
-    return a * b // gcd(a, b)
-
-class Fraction:
-    def __init__(self, num=0, den=1):
-        self.num = num
-        self.den = den
-        self.norm()
-
-    def norm(self):
-        d = gcd(abs(self.num), abs(self.den))
-        self.num //= d
-        self.den //= d
-        if self.den < 0:
-            self.den = -self.den
-            self.num = -self.num
-
-    def __add__(self, o):
-        d = lcm(self.den, o.den)
-        return Fraction(d // self.den * self.num + d // o.den * o.num, d)
-
-    def __sub__(self, o):
-        d = lcm(self.den, o.den)
-        return Fraction(d // self.den * self.num - d // o.den * o.num, d)
-
-    def __mul__(self, o):
-        return Fraction(self.num * o.num, self.den * o.den)
-
-    def __truediv__(self, o):
-        return Fraction(self.num * o.den, self.den * o.num)
-
-    def __str__(self):
-        if self.den == 1:
-            return str(self.num)
-        whole = self.num // self.den
-        if whole != 0:
-            rem = abs(self.num) % self.den
-            return f"{whole} {rem}/{self.den}"
-        return f"{self.num}/{self.den}"
-
-def parse(s):
+def parse_fraction(s):
+    """Парсит дробь и возвращает (числитель, знаменатель)."""
     s = s.strip()
+    negative = s.startswith('-')
+    if negative:
+        s = s[1:]
+
     if ' ' in s:
-        parts = s.split(' ', 1)
-        whole = int(parts[0])
-        num, den = map(int, parts[1].split('/'))
-        if den * whole < 0:
-            num = -num + den * whole
-        else:
-            num = num + den * whole
-        return Fraction(num, den)
+        # Формат: "целая числитель/знаменатель"
+        whole_str, frac_str = s.split(' ', 1)
+        whole = int(whole_str)
+        num, den = map(int, frac_str.split('/'))
+        num = whole * den + num
     elif '/' in s:
+        # Формат: "числитель/знаменатель"
         num, den = map(int, s.split('/'))
-        return Fraction(num, den)
     else:
-        return Fraction(int(s))
+        # Целое число
+        num = int(s)
+        den = 1
 
-a = parse(input())
+    if negative:
+        num = -num
+
+    return num, den
+
+
+def format_fraction(num, den):
+    """Форматирует дробь num/den в нужный формат."""
+    if den < 0:
+        num, den = -num, -den
+
+    g = gcd(abs(num), den)
+    num //= g
+    den //= g
+
+    negative = num < 0
+    num = abs(num)
+
+    whole = num // den
+    remainder = num % den
+
+    result = ''
+    if negative and (whole > 0 or remainder > 0):
+        result += '-'
+
+    if remainder == 0:
+        result += str(whole)
+    else:
+        if whole > 0:
+            result += str(whole) + ' '
+        result += f'{remainder}/{den}'
+
+    return result
+
+
+def apply_op(n1, d1, op, n2, d2):
+    if op == '+':
+        num = n1 * d2 + n2 * d1
+        den = d1 * d2
+    elif op == '-':
+        num = n1 * d2 - n2 * d1
+        den = d1 * d2
+    elif op == '*':
+        num = n1 * n2
+        den = d1 * d2
+    elif op == '/':
+        num = n1 * d2
+        den = d1 * n2
+    return num, den
+
+
+line1 = input()
 op = input().strip()
-b = parse(input())
+line2 = input()
 
-ops = {'+': a + b, '-': a - b, '*': a * b, '/': a / b}
-print(ops[op])
+n1, d1 = parse_fraction(line1)
+n2, d2 = parse_fraction(line2)
+
+num, den = apply_op(n1, d1, op, n2, d2)
+print(format_fraction(num, den))
