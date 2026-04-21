25614e704bf4937056a0e7d81041e51d9217141f
PepelNaTishke <chpunkpas09@gmail.com>
2026-04-21 14:52:39 +0300

    Update BY.py

--- BY.py
+++ BY.py
@@ -1,14 +1,34 @@
 n, s = map(int, input().split())
 
-def f(x):
-    return x * (x - 1) // 2
-
-amount = 0
+ans = 0
 for nz in range(n + 1):
-    for m in range(nz + 1):
-        plus = nz - m
-        if f(m) + f(plus) - plus * m == s:
-            amount += 1
+    rest = n - nz
+    if rest == 0:
+        if s == 0:
+            ans += 1
+        continue
+
+    R = rest * (rest - 1) - 2 * s
+    if R < 0 or R % 4 != 0:
+        continue
+
+    T = R // 4
+
+    D = rest * rest - 4 * T
+    if D < 0:
+        continue
+    sd = int(D**0.5 + 0.5)
+    if sd * sd != D:
+        continue
+    m1 = (rest - sd) // 2
+    m2 = (rest + sd) // 2
+    for m in [m1, m2]:
+        if m < 0 or m > rest:
+            continue
+        p = rest - m
+        total = m * (m - 1) // 2 + p * (p - 1) // 2 - m * p
+        if total == s:
+            ans += 1
             break
 
-print(amount)
+print(ans)
